6980ee11d937005326745caad81a0a279a80dad0
stokesdy <stokesdy@msu.edu>
2014-11-12 18:01:18 -0500

    Original Source

---
+++ notice.php
@@ -0,0 +1,33 @@
+<html>
+<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+<title>Notice</title>
+<style type="text/css">
+<!--
+span.cls_004{font-family:Arial,serif;font-size:18.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_004{font-family:Arial,serif;font-size:18.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_007{font-family:Arial,serif;font-size:14.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_007{font-family:Arial,serif;font-size:14.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+-->
+</style>
+</head>
+<body>
+<div style="position:absolute;left:50%;margin-left:-306px;top:0px;width:612px;height:792px;border-style:outset;overflow:hidden">
+<div style="position:absolute;left:0px;top:0px">
+<img src="background.jpg" width=612 height=792></div>
+<div style="position:absolute;left:230.54px;top:249.59px" class="cls_004"><span class="cls_004">East Six Residents</span></div>
+<div style="position:absolute;left:105.82px;top:303.83px" class="cls_007"><span class="cls_007">We designed the turkey as a creative outlet for public expression</span></div>
+<div style="position:absolute;left:102.00px;top:323.27px" class="cls_007"><span class="cls_007">And all of your comments help add to the great culture of this floor</span></div>
+<div style="position:absolute;left:135.27px;top:342.71px" class="cls_007"><span class="cls_007">East Six prides itself on fostering a sociable community</span></div>
+<div style="position:absolute;left:137.35px;top:381.35px" class="cls_007"><span class="cls_007">We understand the humorous intent of your comments</span></div>
+<div style="position:absolute;left:136.87px;top:400.55px" class="cls_007"><span class="cls_007">But those regarding women’s bodies have undermined</span></div>
+<div style="position:absolute;left:111.67px;top:419.99px" class="cls_007"><span class="cls_007">The welcoming environment we have worked so hard to create</span></div>
+<div style="position:absolute;left:206.37px;top:458.63px" class="cls_007"><span class="cls_007">Understand that your comments</span></div>
+<div style="position:absolute;left:222.41px;top:477.83px" class="cls_007"><span class="cls_007">Do isolate other individuals</span></div>
+<div style="position:absolute;left:171.42px;top:516.47px" class="cls_007"><span class="cls_007">East Six is the most admired floor in Wilson</span></div>
+<div style="position:absolute;left:117.10px;top:535.91px" class="cls_007"><span class="cls_007">People frequent this space because they prefer our company</span></div>
+<div style="position:absolute;left:80.17px;top:574.55px" class="cls_007"><span class="cls_007">As a resident, remember that you are the driving force behind this culture</span></div>
+<div style="position:absolute;left:129.43px;top:593.99px" class="cls_007"><span class="cls_007">Be welcoming and help cultivate memories worth sharing</span></div>
+</div>
+
+</body>
+</html>
