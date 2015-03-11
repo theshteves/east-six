4fc276397d3d8b30e85acd3dc8f856054e90a0c0
theshteves <kneisers@msu.edu>
2015-03-11 16:20:35 -0400

    Monthly update

--- js/index.js
+++ js/index.js
@@ -1,33 +1,21 @@
 $(document).ready( function() {
-
     $('#particles').particleground({
 	dotColor: '#ffffff',
 	lineColor: '#5cbdaa'
     });
-
-    $('#greeting').typewrite({
-	'delay': 50,
-	'extra_char': ''
-    });
-
-    $("#wallpaper").fadeOut(1000);
-    $("#greeting").fadeOut(1000);
-
     /*
+    $("#wallpaper").fadeOut(1);
+    $("#join1").fadeOut(1);
     $("header").fadeOut(1);
     $("main").toggle("slide", {direction: "up", distance: 100}, 1);
-    $("#greeting").fadeOut(1);
-    */
+    $("h2").fadeOut(1);
 
-    $("#greeting").show("clip", 1000);
-    $("#wallpaper").delay(2500).fadeIn(2000);
-
-    /*
+    $("h2").show("clip", 1000);
     $("header").delay(1500).show("bounce", 1000);
+    $("#wallpaper").delay(2500).fadeIn(2000);
     $("#join1").delay(3000).fadeIn(1000);
     $("main").delay(3000).effect("slide", {direction: "down", distance: 100}, 1000);
-    */
-    /*
+
     $(window).on("scroll", function() {
 	var navHeight = $(window).height() - 80;
 	if ($(window).scrollTop() > navHeight) {
@@ -40,5 +28,5 @@ $(document).ready( function() {
     $("#join1").click( function() {
 	$("main").effect("bounce", {times: 1}, 500);
     });
-	 */
+		   */
 });
